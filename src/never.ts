1c65ad6557b04d2e33a8826d0ab025fb7e87620e
Yota Adachi <yota961219@gmail.com>
2022-06-11 15:14:35 +0900

    type of never

---
+++ src/never.ts
@@ -0,0 +1,15 @@
+export {};
+
+function error(message: string): never {
+  throw new Error(message);
+}
+
+try {
+  let result = error('エラーが発生しました!');
+  console.log(result);
+} catch (error) {
+  console.log({error});
+}
+
+let foo: void = undefined;
+let bar: never = error('only me!');
