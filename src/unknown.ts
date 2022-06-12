c35f5b649f3296c6e83f766bf1c2e50a60c81211
Yota Adachi <yota961219@gmail.com>
2022-06-12 21:32:51 +0900

    type of unknows

---
+++ src/unknown.ts
@@ -0,0 +1,13 @@
+export {};
+
+const kansu = (): number => 43;
+
+let numberAny: any = kansu();
+let numberUnknown: unknown = kansu();
+
+let sumAny = numberAny + 10;
+
+// タイプガード
+if(typeof numberUnknown === 'number') {
+  let sumUnknown = numberUnknown + 10;
+}
