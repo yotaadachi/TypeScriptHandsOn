c98967cc20b424269d9ff33a8f526aa5c9fbefa9
Yota Adachi <yota961219@gmail.com>
2022-06-12 16:32:58 +0900

    type of aliases

---
+++ src/type-aliases.ts
@@ -0,0 +1,24 @@
+export {};
+
+type Mojiretsu = string;
+
+const fooString: string = 'Hello';
+const fooMojiretsu: Mojiretsu = 'Hello';
+
+const example1 = {
+  name: 'Ham',
+  age: 43
+};
+
+type Profile = {
+  name: string;
+  age: number;
+};
+
+const example2: Profile = {
+  name: 'Ham',
+  age: 43
+};
+
+// example1の型を、Profile2として使用することを宣言。
+type Profile2 = typeof example1;
