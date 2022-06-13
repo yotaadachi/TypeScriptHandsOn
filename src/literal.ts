3d8cd147b1e274a168e87f64b59ee097a7568c11
Yota Adachi <yota961219@gmail.com>
2022-06-13 11:36:14 +0900

    type of literal

---
+++ src/literal.ts
@@ -0,0 +1,11 @@
+export {};
+
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土' | '日' = '日';
+dayOfTheWeek = '月';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 12;
+
+let TRUE: true = true;
+
+
