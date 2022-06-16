1792829509602efe49d22fe2136dc586d2f6426f
Yota Adachi <yota961219@gmail.com>
2022-06-16 15:03:55 +0900

    acces modify of typescript

--- src/access-modifiers.ts
+++ src/access-modifiers.ts
@@ -1,34 +1,16 @@
 export {};
 
 class Person {
-  public name: string;
-  // private age: number;
-  protected age: number;
-  protected nationality: string;
-
-  constructor(name: string, age: number, nationality: string) {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
-    this.nationality = nationality;
   }
 
   profile(): string {
     return `name: ${this.name}, age: ${this.age}`;
   }
 }
-
-class Android extends Person {
-  constructor(name: string, age: number, nationality: string) {
-    super(name, age, nationality);
-  }
-
-  profile(): string {
-    return `name: ${this.name}, age: ${this.age}, nationality: ${this.nationality}`;
-  }
-}
-
-let taro = new Person('Taro', 30, 'Japan');
+let taro = new Person('Taro', 30);
 console.log(taro.profile());
-console.log(taro.name);
-// console.log(taro.age);
-// let hanako = new Person();
