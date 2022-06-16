f9d5c9a84b59a7ac679939551801d29bfdb3ff63
Yota Adachi <yota961219@gmail.com>
2022-06-16 16:29:25 +0900

    getter and setter of typescript

---
+++ src/getter-and-setter.ts
@@ -0,0 +1,43 @@
+export {};
+
+/**
+ * owner
+ *  所有者
+ *  初期化時に設定できる。
+ *  途中で変更できない。
+ *  参照できる。
+ * secretNumber
+ *  個人番号
+ *  初期化時に設定できる。
+ *  途中で変更できる。
+ *  参照できない。
+ */
+
+class MyNumberCard {
+  private _owner: string;
+  private _secretNumber: number;
+
+  constructor(owner: string, secretNumber: number) {
+    this._owner = owner;
+    this._secretNumber = secretNumber;
+  }
+
+  get owner() {
+    return this._owner;
+  }
+
+  set secretNumber(secretNumber: number) {
+    this._secretNumber = secretNumber;
+  }
+
+  debugPrint() {
+    return `secretNumber ${this._secretNumber}`;
+  }
+}
+
+let card = new MyNumberCard('はむさん', 12345678910);
+console.log(card.debugPrint());
+card.secretNumber = 1111111111;
+console.log(card.debugPrint());
+// card.owner = 'Ham'
+console.log(card.owner);
