e34f2ec6fb4bb597702f598bddaf7fe025bff3b0
Yota Adachi <yota961219@gmail.com>
2022-06-18 15:50:39 +0900

    index signature of typescript

---
+++ src/index-signatures.ts
@@ -0,0 +1,20 @@
+export {};
+
+interface Profile {
+  name: string;
+  underTwenty: boolean;
+  [index: string]: string | number | boolean;
+}
+
+let profile: Profile = {
+  name: 'Ham',
+  underTwenty: false
+};
+
+/**
+ * How to write index signatures
+ * { [ index: typeFor Index ]: typeForValue }
+ */
+profile.name = 'Ham';
+profile.age = 43;
+profile.nationality = 'Japan';
