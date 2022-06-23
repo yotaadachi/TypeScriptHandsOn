d385bc78d7b2e4d491adfc61c51287f4db7e4cc7
Yota Adachi <yota961219@gmail.com>
2022-06-23 19:52:04 +0900

    ExcludeExractNumNullableDefinition of TypeScript

---
+++ src/exclude-extract-nonnullable.ts
@@ -0,0 +1,13 @@
+export {};
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+type DebugType = () => void;
+type SomeTypes = string | number | DebugType;
+type FunctionType = Exclude<SomeTypes, string | number>;
+type MyFunctionType = MyExclude<SomeTypes, string | number>
+
+type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
+
+type NullableTypes = string | number | null | undefined;
+type NonNullableTypes = NonNullable<NullableTypes>;
